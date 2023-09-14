61d02a699379f215929718e39b342fa3a86db995
Chareun Francisco <franchareu@hotmail.com>
2023-09-14 15:50:33 -0300

    Se realizaron modificaciones en las card que se renderiza y en los estilos

--- public/js/home.js
+++ public/js/home.js
@@ -1,15 +1,16 @@
 const mostrarPublicaciones = (publicaciones, elementoHtml) => {
 
-    let secciones = "";
-    publicaciones.forEach( (pub) => {
-        secciones +=   `
-        <div class="card mb-3   " style="max-width: 100%;">
+  let secciones = "";
+  publicaciones.forEach((pub) => {
+    secciones +=
+      `
+        <div class="card mb-3" style="max-width: 100%;">
         <div class="row g-0">
           <div class="col-md-4 img-conteiner">
             <img src="${pub.url_imagen}" class="img-fluid rounded-start" alt="${pub.titulo}">
           </div>
           <div class="col-md-8">
-            <div class="card-body">
+            <div class="card-body text-container">
               <h5 class="card-title">${pub.titulo}</h5>
               <p class="card-text">${pub.detalle}</p>
               <p class="card-text"><small class="text-muted">${pub.fecha}</small></p>
@@ -17,22 +18,22 @@ const mostrarPublicaciones = (publicaciones, elementoHtml) => {
           </div>
         </div>
       </div>`
-        
-    })
 
-
-  
+  })
 
 
 
-    elementoHtml.innerHTML = secciones;
-    
+
+
+
+  elementoHtml.innerHTML = secciones;
+
 }
 
 const obtenerPublicaicones = async () => {
-    const response = await fetch('/publicaciones')
-    const data = await response.json()
-    return data;
+  const response = await fetch('/publicaciones')
+  const data = await response.json()
+  return data;
 }
 
 
@@ -40,8 +41,8 @@ const obtenerPublicaicones = async () => {
 
 
 document.addEventListener('DOMContentLoaded', async () => {
-    
-    const publicaciones = await obtenerPublicaicones()
-    const main = document.querySelector('#lista-publicaciones')
-    mostrarPublicaciones(publicaciones, main)
+
+  const publicaciones = await obtenerPublicaicones()
+  const main = document.querySelector('#lista-publicaciones')
+  mostrarPublicaciones(publicaciones, main)
 })
